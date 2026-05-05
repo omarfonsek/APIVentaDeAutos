164199597fe7c25a6c5c0754b4dbe65d5830aba2
omarfonsek123 <omarfonseca309555@correo.itm.edu.co>
2026-05-05 13:21:51 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,36 @@
+using APIVentaDeAutos.Application.Services;
+using APIVentaDeAutos.Domain.Interfaces;
+using APIVentaDeAutos.Infrastructure;
+using APIVentaDeAutos.Infrastructure.Repositories;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("SqlServer")));
+builder.Services.AddScoped<ICarRepository, CarRepository>();
+builder.Services.AddScoped<CarService>();
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+	app.UseSwagger();
+	app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
