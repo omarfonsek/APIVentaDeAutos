87280fd5981e45298b57e9dba2c2615f0f8198f5
omarfonsek123 <omarfonseca309555@correo.itm.edu.co>
2026-05-06 16:21:25 -0500

    Revert "add: Clean architecture layers"
    This reverts commit 64194b247f15dc278f7bdd3ebbdf7697c34e5ed2.

--- Infrastructure/obj/Debug/net8.0/APIVentaDeAutos.Infrastructure.AssemblyInfo.cs
+++ Infrastructure/obj/Debug/net8.0/APIVentaDeAutos.Infrastructure.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("APIVentaDeAutos.Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f2761ef5e67a7c9c43dbadc8cfa3930cee6bf2fd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("APIVentaDeAutos.Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("APIVentaDeAutos.Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
